a5a29e458b88d97c844a10c46b792b25a6aac16c
WENKAI REN <22199563+wenkairen@users.noreply.github.com>
2018-04-27 18:00:10 +0800

    Update PID.cpp

--- CarND-PID-Control-Project/src/PID.cpp
+++ CarND-PID-Control-Project/src/PID.cpp
@@ -11,46 +11,22 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double kp, double ki, double kd) {
     // inintialzie the value
-    this->Kp = Kp;
-    this->Ki = Ki;
-    this->Kd = Kd;
+  Kp = kp;
+  Ki = ki;
+  Kd = kd;
 
-    p_error = 0.0;
-    i_error = 0.0;
-    d_error = 0.0;
-    prev_cte = 0.0;
-  
-    iteration= 1;
-    steps = 50;
-    eval_steps =100; 
-    err = 0;
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
 
-  if (iteration == 1) {
-    prev_cte = cte;
-  }
-
-  double diff_cte = cte - prev_cte;
-  p_error = cte;
+  d_error = cte - p_error;
   i_error += cte;
-  d_error = diff_cte;
-  prev_cte = cte;
-
-  int eval_a = iteration % steps;
-  int eval_b = iteration % (steps+eval_steps);
-
-  if ( eval_b > steps) {
-    err += cte * cte;
-  }
-  // stop condition
-  if ( iteration == (steps + eval_steps)) {
-    cout << "best err : " << err << endl;
-  }
-  iteration ++;
+  p_error = cte;
 }
 
 double PID::TotalError() {
